5c51681d7b18035db5b3b987725891ebb2c3dc82
origin <acmuhammadmustafa@gmail.com>
2025-09-26 16:56:31 -0400

    exercise 1 update

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,48 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        String sizeQuestion = ("Would you like a 'regular' or 'large' sandwich?\n"); // Prompts the question about size.
+        System.out.printf(sizeQuestion); // Prints the question.
+        String size = scanner.next();
+        System.out.printf("You chose %s.\n", size); // Declares the size you chose.
+        System.out.println();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double regularPrice = 5.45;
+        double largePrice = 8.95;
+
+        if (size.equals("regular")) { // Declaring price for regular size.
+            System.out.printf("Your total is currently $%.2f.\n", regularPrice);
+            System.out.println();
+        } else { // Declaring price for large size.
+            System.out.printf("Your total is currently $%.2f.\n", largePrice);
+            System.out.println();
+        }
+
+
+        String ageQuestion = ("How old are you?\n");
+        System.out.println(ageQuestion);
+        int age = scanner.nextInt();
+        System.out.println();
+        if (age <= 17 && size.equals("regular")) {
+            System.out.printf("Your total is $%.2f!",regularPrice*0.9);
+        } else if (age <= 17 && size.equals("large")) {
+            System.out.printf("Your total is $%.2f!",largePrice*0.9);
+        } else if (age >= 18 && size.equals("regular")) {
+            System.out.println("Your total is $" + regularPrice + "!");
+
+        }
+
+        if (age >= 65 && size.equals("regular")) {
+            System.out.printf(("Your total is %.2f!"), regularPrice * 0.8);
+        } else if (age >= 65 && size.equals("large")) {
+            System.out.printf(("Your total is %.2f!"), largePrice * 0.8);
+        } else if (age >= 18 && age <=64 && size.equals("large")){
+            System.out.println("Your total is $" + largePrice + "!");
         }
     }
 }
