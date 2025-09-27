5a59370ed8178eb0e168489f185b6b6ed4e4aeae
origin <acmuhammadmustafa@gmail.com>
2025-09-26 23:26:26 -0400

    exercise 2 update

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -14,35 +14,60 @@ public class Main {
 
         double regularPrice = 5.45;
         double largePrice = 8.95;
+        double basePrice;
 
         if (size.equals("regular")) { // Declaring price for regular size.
-            System.out.printf("Your total is currently $%.2f.\n", regularPrice);
+            basePrice = regularPrice;
+            System.out.printf("Your total is currently $%.2f.\n", basePrice);
             System.out.println();
         } else { // Declaring price for large size.
-            System.out.printf("Your total is currently $%.2f.\n", largePrice);
+            basePrice = largePrice;
+            System.out.printf("Your total is currently $%.2f.\n", basePrice);
             System.out.println();
         }
+
+        String loadQuestion = ("Would you like your sandwich loaded? N = No. YA = Yes, regular! YB = Yes, large!");
+        System.out.printf(loadQuestion);
+        String loaded = scanner.next();
+        System.out.printf("You chose %s.\n", loaded);
+        System.out.println();
+
+        double N = 0 + basePrice;
+        double YR = 1.00 + basePrice ;
+        double YL = 1.75 + basePrice;
+        double updatedPrice = 0.0;
+        if (loaded.equals("N")) {
+            System.out.println("No additional charge.");
+            updatedPrice = N;
+        } else if (loaded.equals("YA")) {
+            System.out.printf("Your total is now $%.2f.\n", YR);
+            updatedPrice = YR;
+        } else if (loaded.equals("YB")) {
+            System.out.printf("Your total is now $%.2f.\n", YL);
+            updatedPrice = YL;
+        }
+
 
 
         String ageQuestion = ("How old are you?\n");
-        System.out.println(ageQuestion);
-        int age = scanner.nextInt();
-        System.out.println();
-        if (age <= 17 && size.equals("regular")) {
-            System.out.printf("Your total is $%.2f!",regularPrice*0.9);
-        } else if (age <= 17 && size.equals("large")) {
-            System.out.printf("Your total is $%.2f!",largePrice*0.9);
-        } else if (age >= 18 && size.equals("regular")) {
-            System.out.println("Your total is $" + regularPrice + "!");
+            System.out.println(ageQuestion);
+            int age = scanner.nextInt();
+            System.out.println();
+            if (age <= 17 && size.equals("regular")) {
+                System.out.printf("Your total is $%.2f!", updatedPrice * 0.9);
+            } else if (age <= 17 && size.equals("large")) {
+                System.out.printf("Your total is $%.2f!",updatedPrice * 0.9);
+            } else if (age >= 18 && age <= 64 && size.equals("regular")) {
+                System.out.println("Your total is $" + updatedPrice + "!");
 
-        }
+            }
 
-        if (age >= 65 && size.equals("regular")) {
-            System.out.printf(("Your total is %.2f!"), regularPrice * 0.8);
-        } else if (age >= 65 && size.equals("large")) {
-            System.out.printf(("Your total is %.2f!"), largePrice * 0.8);
-        } else if (age >= 18 && age <=64 && size.equals("large")){
-            System.out.println("Your total is $" + largePrice + "!");
+            if (age >= 65 && size.equals("regular")) {
+                System.out.printf(("Your total is $%.2f!"), updatedPrice * 0.8);
+            } else if (age >= 65 && size.equals("large")) {
+                System.out.printf(("Your total is %.2f!"), updatedPrice * 0.8);
+            } else if (age >= 18 && age <= 64 && size.equals("large")) {
+                System.out.println("Your total is $" + updatedPrice + "!");
+            }
         }
     }
-}
